ade0d4a09a16c02b8aa7e3b03ebbf56682a5ef29
Ashraf Hafiz <Ashraf.Hafiz@yahoo.com>
2019-08-20 18:27:13 +0200

    a small rearrange of inventory buttons

--- src/components/inventory/button.component.jsx
+++ src/components/inventory/button.component.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Button = props => {
+  const { onSelect, buttonTitle, checked } = props;
+
+  const handleClick = () => {
+    onSelect();
+  };
+
   return (
-    <button className="btn btn-outline-secondary text-uppercase mx-1">
-      {props.buttonTitle}
+    <button
+      className="btn btn-outline-secondary text-uppercase mx-1"
+      onClick={() => handleClick()}
+    >
+      {buttonTitle}
     </button>
   );
 };
--- src/components/inventory/inventory.component.jsx
+++ src/components/inventory/inventory.component.jsx
@@ -1,31 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import "./inventory.styles.css";
 import SectionTitle from "../section-title/section-title.component";
 import InventoryItem from "./inventory-item.component";
 import Button from "./button.component";
 
-const Inventory = props => {
-  return (
-    <section className="inventory py-5" id="inventory">
-      <div className="container">
-        <SectionTitle title1="Our" title2="Inventory" />
+export default class Inventory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayInventory: this.props.inventoryCars
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-        <div className="row mb-5">
-          <div className="col-10 mx-auto col-md-12 d-flex justify-content-end">
-            <Button buttonTitle="All" />
-            <Button buttonTitle="American" />
-            <Button buttonTitle="German" />
+  handleSelect = filterType => {
+    console.log(filterType);
+    switch (filterType) {
+      case "all":
+        this.setState({
+          displayInventory: this.props.inventoryCars
+        });
+        break;
+      case "german":
+        this.setState({
+          displayInventory: this.props.inventoryCars.filter(
+            car => car.country === "German"
+          )
+        });
+        break;
+      case "american":
+        this.setState({
+          displayInventory: this.props.inventoryCars.filter(
+            car => car.country === "American"
+          )
+        });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  render() {
+    const { displayInventory } = this.state;
+    return (
+      <section className="inventory py-5" id="inventory">
+        <div className="container">
+          <SectionTitle title1="Our" title2="Inventory" />
+
+          <div className="row mb-5">
+            <div className="col-10 mx-auto col-md-12 d-flex justify-content-end">
+              <Button
+                buttonTitle="All"
+                onSelect={() => this.handleSelect("all")}
+              />
+              <Button
+                buttonTitle="American"
+                onSelect={() => this.handleSelect("american")}
+              />
+              <Button
+                buttonTitle="German"
+                onSelect={() => this.handleSelect("german")}
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            {displayInventory.map((car, index) => (
+              <InventoryItem key={index} {...car} />
+            ))}
           </div>
         </div>
-
-        <div className="row">
-          {props.inventoryCars.map((car, index) => (
-            <InventoryItem key={index} {...car} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default Inventory;
+      </section>
+    );
+  }
+}
